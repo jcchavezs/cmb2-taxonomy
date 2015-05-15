1eb4335da09eee893b26917df324704fe23787e2
jcchavezs <jcchavezs@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 08:01:47 +0000

    Changed __DIR__ to dirname(__FILE__) due to retrocompatibility of the plugin with PHP 5.2.* and above.
    git-svn-id: http://plugins.svn.wordpress.org/cmb2-taxonomy/trunk@1160980 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- init.php
+++ init.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/functions.php';
-require_once __DIR__ . '/includes/CMB2_Taxonomy.php';
+require_once dirname(__FILE__) . '/functions.php';
+require_once dirname(__FILE__) . '/includes/CMB2_Taxonomy.php';
 
 new CMB2_Taxonomy();
